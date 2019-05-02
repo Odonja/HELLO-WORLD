e4e0dbc7f4dfd60239e8262ece954495b1ceae71
Anneke <huijsmans90@gmail.com>
2019-05-02 14:15:57 +0200

    Add files via upload

---
+++ src/test/java/org/anneke/exercise/AppTest.java
@@ -0,0 +1,15 @@
+package org.anneke.exercise;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AppTest {
+	
+	@Test
+    public void testApp()
+    {
+		int verify = 280;
+		assertEquals(290, verify);
+    }
+}
